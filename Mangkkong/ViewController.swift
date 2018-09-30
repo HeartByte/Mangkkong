20026126f7d8315ce4d8865250b5a51d51ac4cbe
김호중 <khj30687@gmail.com>
2018-10-01 00:03:09 +0900

    Initial Commit

---
+++ Mangkkong/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mangkkong
+//
+//  Created by 김호중 on 01/10/2018.
+//  Copyright © 2018 김호중. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
