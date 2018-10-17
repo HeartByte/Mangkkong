96ca4baadf4c9f554d7201ade7fdca39c3ca55b9
김효은 <ag2665@naver.com>
2018-10-17 22:15:56 +0900

    menu UI 기본생성

---
+++ Mangkkong/MenuController.swift
@@ -0,0 +1,28 @@
+//
+//  MenuController.swift
+//  Mangkkong
+//
+//  Created by 김효은 on 17/10/2018.
+//  Copyright © 2018 김호중. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func myRoom(_ sender: Any, forEvent event: UIEvent) {
+    }
+   
+    @IBAction func toDo(_ sender: Any, forEvent event: UIEvent) {
+    }
+    
+    @IBAction func calendar(_ sender: Any, forEvent event: UIEvent) {
+    }
+    
+    @IBAction func timeline(_ sender: Any, forEvent event: UIEvent) {
+    }
+    
+}
